844941accc7a681cc9da039c8548b6bfb1955e13
Ian MacDonald & Sunny Zhang <student@epicodus.com>
2015-04-22 13:28:13 -0700

    Add tests for scissors case.

--- spec/r_p_s_spec.rb
+++ spec/r_p_s_spec.rb
@@ -2,10 +2,18 @@ require('rspec')
 require('r_p_s')
 
 describe('String#beats?') do
+  #rock tests
   it("will return true when rock beats scissors") do
     expect('rock'.beats?('scissors')).to(eq(true))
   end
   it("will return false when rock loses to paper") do
     expect('rock'.beats?('paper')).to(eq(false))
+  end
+  #scissors tests
+  it("will return true when scissors beats paper") do
+    expect('scissors'.beats?('paper')).to(eq(true))
+  end
+  it("will return false when scissors loses to rock") do
+    expect('scissors'.beats?('rock')).to(eq(false))
   end
 end
