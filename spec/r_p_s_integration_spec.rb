6fea884a29d39f1b0b1c6f42b1e7e86cdd194112
Ian MacDonald & Sunny Zhang <student@epicodus.com>
2015-04-22 14:47:39 -0700

    Integration test written and failed.

---
+++ spec/r_p_s_integration_spec.rb
@@ -0,0 +1,15 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+
+describe('the rock paper scissors game path', {:type => :feature}) do
+  it('process the users\' entries and returns the result of the game') do
+    visit('/')
+    fill_in('player1', :with => 'rock')
+    click_button('Send')
+    expect(page).to have_content('Player 2\'s move')
+    fill_in('player2', :with => 'scissors')
+    click_button('Who won?')
+    expect(page).to have_content('Player 1 won!')
+  end
+end
