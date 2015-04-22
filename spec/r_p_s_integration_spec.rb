4e9538fa78d9a36d085277fed83957522f739d91
Ian MacDonald & Sunny Zhang <student@epicodus.com>
2015-04-22 15:45:57 -0700

    Pass integration testing for player 1 rock and player 2 scissors.

--- spec/r_p_s_integration_spec.rb
+++ spec/r_p_s_integration_spec.rb
@@ -6,7 +6,8 @@ describe('the rock paper scissors game path', {:type => :feature}) do
   it('process the users\' entries and returns the result of the game') do
     visit('/')
     fill_in('player1', :with => 'rock')
+    fill_in('player2', :with => 'scissors')
     click_button('Send')
-    expect(page).to have_content('Player 1 won!')
+    expect(page).to have_content('Player 1 Wins!')
   end
 end
