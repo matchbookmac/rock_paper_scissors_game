84e5899354a044966c01c33898fc16f8a905134a
Ian MacDonald & Sunny Zhang <student@epicodus.com>
2015-04-22 12:12:33 -0700

    returns true when called on rock with an argument of scissors

--- lib/r_p_s.rb
+++ lib/r_p_s.rb
@@ -1,5 +1,15 @@
 class String
   define_method(:beats?) do |play|
-    #some code
+    player1 = self
+    player2 = play
+    if(player1.eql?('rock'))
+      if(player2.eql?('scissors'))
+        true
+      elsif(player2.eql?('paper'))
+        false
+      end
+    else
+      false
+    end
   end
 end
--- spec/r_p_s_spec.rb
+++ spec/r_p_s_spec.rb
@@ -5,4 +5,7 @@ describe('String#beats?') do
   it("will return true when rock beats scissors") do
     expect('rock'.beats?('scissors')).to(eq(true))
   end
+  it("will return false when rock loses to paper") do
+    expect('rock'.beats?('paper')).to(eq(false))
+  end
 end
