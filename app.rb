783f749e3fcc7b8cdf855e9ea257e393f875f78e
Ian MacDonald & Sunny Zhang <student@epicodus.com>
2015-04-22 14:52:30 -0700

    Pass integration testing for two players submitting moves

--- app.rb
+++ app.rb
@@ -2,3 +2,18 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/r_p_s')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get('/player_2') do
+  @player1 = params.fetch('player1')
+  erb(:player_2)
+end
+
+get('/result') do
+  @player2 = params.fetch('player2')
+  @result = @player1.beats?(@player2)
+  erb(:result)
+end
